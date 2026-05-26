d57d3f2251a095849701254d01e2a85cfbee1bb5
DaniSsj2442 <daniels.pereiraa@autonoma.edu.co>
2026-05-25 23:08:17 -0500

    Create config.example.h

---
+++ esp32/config.example.h
@@ -0,0 +1,11 @@
+const char* ssid = "TU_WIFI";
+
+const char* password = "TU_PASSWORD";
+
+const char* mqtt_server = "TU_HOSTNAME.local";
+
+const int mqtt_port = 8883;
+
+const char* mqtt_user = "TU_USUARIO";
+
+const char* mqtt_pass = "TU_PASSWORD";
