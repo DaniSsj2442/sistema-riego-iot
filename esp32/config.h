d2eb5b2efabd90e7e662792e40bb430b0d5b62bb
DaniSsj2442 <daniels.pereiraa@autonoma.edu.co>
2026-05-25 23:09:32 -0500

    Create config.h

---
+++ esp32/config.h
@@ -0,0 +1,11 @@
+const char* ssid = "TU_WIFI_REAL";
+
+const char* password = "TU_PASSWORD_REAL";
+
+const char* mqtt_server = "daniel-VirtualBox.local";
+
+const int mqtt_port = 8883;
+
+const char* mqtt_user = "Tu_Usuario";
+
+const char* mqtt_pass = "Tu_Password";
